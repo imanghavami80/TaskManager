23d0c4663d3b844e76b0fef8f0e55cee8e96e2bf
iman <imanqhavami.80@gmail.com>
2024-12-06 01:39:38 +0330

    We wanna start talking about REST.

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  root 'main#index'
+
+  match 'about', to: 'main#about', via: :get
+
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
+  get 'tasks/delete'
+
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/edit'
+  get 'categories/delete'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
